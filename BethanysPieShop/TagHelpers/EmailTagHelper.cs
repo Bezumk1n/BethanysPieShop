e3cd94fa6a93e5a2fdfe0f6a123276fa19bb3a5b
Umnov Stanislav <umnov.msk@gmail.com>
2024-05-19 21:32:02 +0300

    Добавлены Unit тесты

--- BethanysPieShop/TagHelpers/EmailTagHelper.cs
+++ BethanysPieShop/TagHelpers/EmailTagHelper.cs
@@ -4,12 +4,12 @@ namespace BethanysPieShop.TagHelpers
 {
     public class EmailTagHelper:TagHelper 
     {
-        public string? Adress { get; set; }
+        public string? Address { get; set; }
         public string? Content { get; set; }
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
-            output.Attributes.SetAttribute("href", "mailto:" + Adress);
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
             output.Content.SetContent(Content);
         }
     }
